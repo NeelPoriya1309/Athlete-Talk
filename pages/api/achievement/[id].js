309b920b06814325e4def665c86ea324a5727695
NeelPoriya1309 <202001139@daiict.ac.in>
2023-04-26 18:11:24 +0530

    done done

--- pages/api/achievement/[id].js
+++ pages/api/achievement/[id].js
@@ -1,9 +1,11 @@
 const nc = require('next-connect');
 const authController = require('../../../controllers/authController');
-const catchAsync = require('./../../../utils/catchAsync');
+const catchAsync = require('../../../utils/catchAsync');
 const PDFDocument = require('pdfkit');
+const Plan = require('../../../models/planModel');
+const dbConnect = require('../../../lib/mongoose');
 
-const buildPDF = (dataCallback, endCallback, chat) => {
+const buildPDF = (dataCallback, endCallback, plan) => {
   const doc = new PDFDocument({
     size: 'C4',
     layout: 'landscape',
@@ -11,9 +13,7 @@ const buildPDF = (dataCallback, endCallback, chat) => {
   doc.on('data', dataCallback);
   doc.on('end', endCallback);
   doc.fontSize(25).text('Congratulations!', 100, 50);
-  doc
-    .fontSize(20)
-    .text('You have completed Take 5 days of your week for this!...', 100, 100);
+  doc.fontSize(20).text(`You have completed ${plan.name}`, 100, 100);
   doc.image('public/achievement.png', 150, 20, { width: 550 });
 
   doc.end();
@@ -25,10 +25,12 @@ const handler = nc({
 });
 
 handler.get(
-  //   authController.protect,
+  // authController.protect,
   catchAsync(async (req, res, next) => {
-    const { chat } = req.body;
+    await dbConnect();
+    const { id } = req.query;
 
+    const plan = await Plan.findById(id);
     const stream = res.writeHead(200, {
       'Content-Type': 'application/pdf',
       'Content-Disposition': `attachment;filename=mypdf.pdf`,
@@ -37,7 +39,7 @@ handler.get(
     buildPDF(
       (data) => stream.write(data),
       () => stream.end(),
-      chat
+      plan
     );
   })
 );
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import { Box, Grid, Icon, Paper, Typography } from '@mui/material';
 import dynamic from 'next/dynamic';
 import AuthContext from '../../store/auth-context';
+import { useRouter } from 'next/router';
+import { Download } from '@mui/icons-material';
 
 const BasicRadialChart = dynamic(
   () => import('../../components/BasicRadialChart'),
@@ -34,7 +36,8 @@ const colors = [
 ];
 
 const Dashboard = ({ plans, categories }) => {
-  console.log(categories);
+  console.log(plans);
+  const router = useRouter();
   const authContext = useContext(AuthContext);
   const completedPlans = plans.filter(
     (plan) => plan.progress === plan.plan.noOfDays
@@ -52,57 +55,94 @@ const Dashboard = ({ plans, categories }) => {
         <Typography variant="h5" p={2}>
           Plan Summary
         </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <BasicRadialChart
-              completed={completedPlans.length}
-              total={plans.length}
-            />
-          </Grid>
-          <Grid item xs={12} md={4} p={2}>
-            <Typography variant="h5">In Progress</Typography>
-            {inProgressPlans.map((plan) => (
-              <Box
-                key={plan._id}
-                sx={{ display: 'flex', justifyContent: 'space-between' }}
-              >
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontWeight: 'bold',
-                  }}
-                >
-                  {plan.plan.name}
-                </Typography>
-                <Typography variant="body1">
-                  {plan.progress}/{plan.plan.noOfDays}
-                </Typography>
-              </Box>
-            ))}
-          </Grid>
-          <Grid item xs={12} md={4} p={2}>
-            <Typography variant="h5">Completed</Typography>
-            {completedPlans.map((plan) => (
-              <Box
-                key={plan._id}
-                sx={{ display: 'flex', justifyContent: 'space-between' }}
-              >
-                <Typography sx={{ fontWeight: 'bold' }} variant="h6">
-                  {plan.plan.name}
-                </Typography>
-                <Typography variant="body1">
-                  {plan.progress}/{plan.plan.noOfDays}
-                </Typography>
-              </Box>
-            ))}
-          </Grid>
-        </Grid>
+        {plans.length === 0 && (
+          <Typography
+            variant="h5"
+            p={2}
+            sx={{ display: 'flex', justifyContent: 'center', pt: 0 }}
+          >
+            No plans yet
+          </Typography>
+        )}
+        {plans.length > 0 && (
+          <>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={4}>
+                <BasicRadialChart
+                  completed={completedPlans.length}
+                  total={plans.length}
+                />
+              </Grid>
+              <Grid item xs={12} md={4} p={2}>
+                <Typography variant="h5">In Progress</Typography>
+                {inProgressPlans.map((plan) => (
+                  <Box
+                    key={plan._id}
+                    sx={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      {plan.plan.name}
+                    </Typography>
+                    <Typography variant="body1">
+                      {plan.progress}/{plan.plan.noOfDays}
+                    </Typography>
+                  </Box>
+                ))}
+              </Grid>
+              <Grid item xs={12} md={4} p={2}>
+                <Typography variant="h5">Completed</Typography>
+                {completedPlans.map((plan) => (
+                  <Box
+                    key={plan._id}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Typography sx={{ fontWeight: 'bold' }} variant="h6">
+                      {plan.plan.name}
+                    </Typography>
+                    <Typography variant="body1">
+                      {plan.progress}/{plan.plan.noOfDays}
+                    </Typography>
+                    <a
+                      key={plan._id}
+                      href={`/api/achievement/${plan.plan._id}`}
+                      target="_black"
+                      style={{
+                        color: 'black',
+                        textDecoration: 'none',
+                      }}
+                    >
+                      <Icon component={Download} sx={{ marginLeft: '20px' }} />
+                    </a>
+                  </Box>
+                ))}
+              </Grid>
+            </Grid>
+          </>
+        )}
       </Paper>
       <br></br>
       <Paper elevation={2}>
         <Typography variant="h5" p={2}>
           Preferred Category
         </Typography>
+        {categories[0].preferredCategories.length === 0 && (
+          <Typography
+            variant="h5"
+            p={2}
+            sx={{ display: 'flex', justifyContent: 'center', pt: 0 }}
+          >
+            No preferred categories yet
+          </Typography>
+        )}
+
         <Box
           sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }}
           m={2}
